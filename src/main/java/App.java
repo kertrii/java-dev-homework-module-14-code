9f2621fced45a342b6ca6732a6b1ce0ba29eaa84
Kateryna <kateryna299@gmail.com>
2024-08-26 15:03:35 +0200

    refactoring

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,90 +1,99 @@
 import java.util.Scanner;
 
 public class App {
+    public static final char PLAYER_MARK = 'X';
+    public static final char COMPUTER_MARK = 'O';
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        byte input;
-        byte rand;
-        byte i;
-        boolean boxAvailable = false;
-        byte winner = 0;
-        char box[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        char[] box = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
         System.out.println("Enter box number to select. Enjoy!\n");
 
         boolean boxEmpty = false;
         while (true) {
-            System.out.println("\n\n " + box[0] + " | " + box[1] + " | " + box[2] + " ");
-            System.out.println("-----------");
-            System.out.println(" " + box[3] + " | " + box[4] + " | " + box[5] + " ");
-            System.out.println("-----------");
-            System.out.println(" " + box[6] + " | " + box[7] + " | " + box[8] + " \n");
+            printBox(box);
+
             if(!boxEmpty){
-                for(i = 0; i < 9; i++)
+                for(byte i = 0; i < 9; i++)
                     box[i] = ' ';
                 boxEmpty = true;
             }
 
-            if(winner == 1){
+            if(isWinner(box, PLAYER_MARK)){
                 System.out.println("You won the game!\nCreated by Shreyas Saha. Thanks for playing!");
                 break;
-            } else if(winner == 2){
+            }
+            if(isWinner(box, COMPUTER_MARK)){
                 System.out.println("You lost the game!\nCreated by Shreyas Saha. Thanks for playing!");
                 break;
-            } else if(winner == 3){
+            }
+            if(isBoxFull(box)){
                 System.out.println("It's a draw!\nCreated by Shreyas Saha. Thanks for playing!");
                 break;
             }
 
-            while (true) {
-                input = scan.nextByte();
-                if (input > 0 && input < 10) {
-                    if (box[input - 1] == 'X' || box[input - 1] == 'O')
-                        System.out.println("That one is already in use. Enter another.");
-                    else {
-                        box[input - 1] = 'X';
-                        break;
-                    }
-                }
-                else
-                    System.out.println("Invalid input. Enter again.");
-            }
+            playerMove(scan, box);
 
-            if((box[0]=='X' && box[1]=='X' && box[2]=='X') || (box[3]=='X' && box[4]=='X' && box[5]=='X') || (box[6]=='X' && box[7]=='X' && box[8]=='X') ||
-               (box[0]=='X' && box[3]=='X' && box[6]=='X') || (box[1]=='X' && box[4]=='X' && box[7]=='X') || (box[2]=='X' && box[5]=='X' && box[8]=='X') ||
-               (box[0]=='X' && box[4]=='X' && box[8]=='X') || (box[2]=='X' && box[4]=='X' && box[6]=='X')){
-                   winner = 1;
-                   continue;
-            }
-
-            boxAvailable = false;
-            for(i=0; i<9; i++){
-                if(box[i] != 'X' && box[i] != 'O'){
-                    boxAvailable = true;
-                    break;
-                }
-            }
-
-            if(boxAvailable == false){
-                winner = 3;
+            if (isWinner(box, PLAYER_MARK) || isBoxFull(box)) {
                 continue;
             }
 
-            while (true) {
-                rand = (byte) (Math.random() * (9 - 1 + 1) + 1);
-                if (box[rand - 1] != 'X' && box[rand - 1] != 'O') {
-                    box[rand - 1] = 'O';
-                    break;
-                }
-            }
+            computerMove(box);
+        }
+    }
 
-            if((box[0]=='O' && box[1]=='O' && box[2]=='O') || (box[3]=='O' && box[4]=='O' && box[5]=='O') || (box[6]=='O' && box[7]=='O' && box[8]=='O') ||
-               (box[0]=='O' && box[3]=='O' && box[6]=='O') || (box[1]=='O' && box[4]=='O' && box[7]=='O') || (box[2]=='O' && box[5]=='O' && box[8]=='O') ||
-               (box[0]=='O' && box[4]=='O' && box[8]=='O') || (box[2]=='O' && box[4]=='O' && box[6]=='O')){
-                winner = 2;
-                continue;
+    private static boolean isBoxFull(char[] box) {
+        for (byte i = 0; i < 9; i++) {
+            if(box[i] != 'X' && box[i] != 'O'){
+                return false;
             }
         }
+        return true;
+    }
 
+    private static void playerMove(Scanner scan, char[] box) {
+        while (true) {
+            byte input = scan.nextByte();
+            if (input > 0 && input < 10) {
+                if (box[input - 1] == 'X' || box[input - 1] == 'O')
+                    System.out.println("That one is already in use. Enter another.");
+                else {
+                    box[input - 1] = 'X';
+                    break;
+                }
+            }
+            else
+                System.out.println("Invalid input. Enter again.");
+        }
+    }
+
+    private static void computerMove(char[] box) {
+        while (true) {
+            byte rand = (byte) (Math.random() * 9 + 1);
+            if (box[rand - 1] != 'X' && box[rand - 1] != 'O') {
+                box[rand - 1] = 'O';
+                break;
+            }
+        }
+    }
+
+    private static boolean isWinner(char[] box, char mark) {
+        return (box[0] == mark && box[1] == mark && box[2] == mark) ||
+                (box[3] == mark && box[4] == mark && box[5] == mark) ||
+                (box[6] == mark && box[7] == mark && box[8] == mark) ||
+                (box[0] == mark && box[3] == mark && box[6] == mark) ||
+                (box[1] == mark && box[4] == mark && box[7] == mark) ||
+                (box[2] == mark && box[5] == mark && box[8] == mark) ||
+                (box[0] == mark && box[4] == mark && box[8] == mark) ||
+                (box[2] == mark && box[4] == mark && box[6] == mark);
+    }
+
+    private static void printBox(char[] box) {
+        System.out.println("\n\n " + box[0] + " | " + box[1] + " | " + box[2] + " ");
+        System.out.println("-----------");
+        System.out.println(" " + box[3] + " | " + box[4] + " | " + box[5] + " ");
+        System.out.println("-----------");
+        System.out.println(" " + box[6] + " | " + box[7] + " | " + box[8] + " \n");
     }
 }
